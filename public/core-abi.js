0b115ea207fa95b37e5b5da3f017374acda89d28
Alex Bakoushin <alex@bakoush.in>
2022-02-07 23:45:07 +0300

    Update ABI

--- public/core-abi.js
+++ public/core-abi.js
@@ -2,243 +2,250 @@
 
 export default [
   {
-    anonymous: false,
-    inputs: [
+    "anonymous": false,
+    "inputs": [
       {
-        indexed: true,
-        internalType: 'address',
-        name: 'evidenceDelegate',
-        type: 'address'
+        "indexed": true,
+        "internalType": "address",
+        "name": "evidenceDelegate",
+        "type": "address"
       }
     ],
-    name: 'EvidenceDelegateAdded',
-    type: 'event'
+    "name": "EvidenceDelegateAdded",
+    "type": "event"
   },
   {
-    anonymous: false,
-    inputs: [
+    "anonymous": false,
+    "inputs": [
       {
-        indexed: true,
-        internalType: 'address',
-        name: 'agent',
-        type: 'address'
+        "indexed": true,
+        "internalType": "address",
+        "name": "agent",
+        "type": "address"
       }
     ],
-    name: 'EvidenceSubmitted',
-    type: 'event'
+    "name": "EvidenceSubmitted",
+    "type": "event"
   },
   {
-    anonymous: false,
-    inputs: [
+    "anonymous": false,
+    "inputs": [
       {
-        indexed: true,
-        internalType: 'address',
-        name: 'previousOwner',
-        type: 'address'
+        "indexed": true,
+        "internalType": "address",
+        "name": "previousOwner",
+        "type": "address"
       },
       {
-        indexed: true,
-        internalType: 'address',
-        name: 'newOwner',
-        type: 'address'
+        "indexed": true,
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
       }
     ],
-    name: 'OwnershipTransferred',
-    type: 'event'
+    "name": "OwnershipTransferred",
+    "type": "event"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'address',
-        name: '',
-        type: 'address'
+        "internalType": "address",
+        "name": "",
+        "type": "address"
       }
     ],
-    name: 'isDelegate',
-    outputs: [
+    "name": "isDelegate",
+    "outputs": [
       {
-        internalType: 'bool',
-        name: '',
-        type: 'bool'
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
       }
     ],
-    stateMutability: 'view',
-    type: 'function',
-    constant: true
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
   },
   {
-    inputs: [],
-    name: 'owner',
-    outputs: [
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
       {
-        internalType: 'address',
-        name: '',
-        type: 'address'
+        "internalType": "address",
+        "name": "",
+        "type": "address"
       }
     ],
-    stateMutability: 'view',
-    type: 'function',
-    constant: true
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'address',
-        name: '',
-        type: 'address'
+        "internalType": "address",
+        "name": "",
+        "type": "address"
       }
     ],
-    name: 'proofOfHumanity',
-    outputs: [
+    "name": "proofOfHumanity",
+    "outputs": [
       {
-        internalType: 'address',
-        name: 'agent',
-        type: 'address'
+        "internalType": "address",
+        "name": "agent",
+        "type": "address"
       },
       {
-        internalType: 'bytes',
-        name: 'timestamp',
-        type: 'bytes'
+        "internalType": "bytes",
+        "name": "timestamp",
+        "type": "bytes"
       },
       {
-        internalType: 'address',
-        name: 'delegate',
-        type: 'address'
+        "internalType": "address",
+        "name": "delegate",
+        "type": "address"
       },
       {
-        internalType: 'bytes',
-        name: 'evidence',
-        type: 'bytes'
+        "internalType": "bytes",
+        "name": "evidence",
+        "type": "bytes"
       }
     ],
-    stateMutability: 'view',
-    type: 'function',
-    constant: true
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
   },
   {
-    inputs: [],
-    name: 'renounceOwnership',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function'
+    "inputs": [],
+    "name": "renounceOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'address',
-        name: 'newOwner',
-        type: 'address'
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
       }
     ],
-    name: 'transferOwnership',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function'
+    "name": "transferOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'delegate',
-        type: 'address'
-      }
-    ],
-    name: 'addEvidenceDelegate',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function'
+    "inputs": [],
+    "name": "initialize",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'address',
-        name: 'agent',
-        type: 'address'
-      },
-      {
-        internalType: 'bytes',
-        name: 'timestamp',
-        type: 'bytes'
-      },
-      {
-        internalType: 'address',
-        name: 'delegate',
-        type: 'address'
-      },
-      {
-        internalType: 'bytes',
-        name: 'evidence',
-        type: 'bytes'
+        "internalType": "address",
+        "name": "delegate",
+        "type": "address"
       }
     ],
-    name: 'submitEvidence',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function'
+    "name": "addEvidenceDelegate",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'bytes',
-        name: 'evidence',
-        type: 'bytes'
-      }
-    ],
-    name: 'splitEvidence',
-    outputs: [
-      {
-        internalType: 'bytes32',
-        name: 'R',
-        type: 'bytes32'
+        "internalType": "address",
+        "name": "agent",
+        "type": "address"
       },
       {
-        internalType: 'bytes',
-        name: 'agentSignature',
-        type: 'bytes'
+        "internalType": "bytes",
+        "name": "timestamp",
+        "type": "bytes"
       },
       {
-        internalType: 'bytes',
-        name: 'delegateSignature',
-        type: 'bytes'
+        "internalType": "address",
+        "name": "delegate",
+        "type": "address"
+      },
+      {
+        "internalType": "bytes",
+        "name": "evidence",
+        "type": "bytes"
       }
     ],
-    stateMutability: 'pure',
-    type: 'function',
-    constant: true
+    "name": "submitEvidence",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: 'address',
-        name: 'agent',
-        type: 'address'
-      },
-      {
-        internalType: 'bytes',
-        name: 'timestamp',
-        type: 'bytes'
-      },
-      {
-        internalType: 'address',
-        name: 'delegate',
-        type: 'address'
-      },
-      {
-        internalType: 'bytes',
-        name: 'evidence',
-        type: 'bytes'
+        "internalType": "bytes",
+        "name": "evidence",
+        "type": "bytes"
       }
     ],
-    name: 'validateEvidence',
-    outputs: [
+    "name": "splitEvidence",
+    "outputs": [
       {
-        internalType: 'bool',
-        name: '',
-        type: 'bool'
+        "internalType": "bytes32",
+        "name": "R",
+        "type": "bytes32"
+      },
+      {
+        "internalType": "bytes",
+        "name": "agentSignature",
+        "type": "bytes"
+      },
+      {
+        "internalType": "bytes",
+        "name": "delegateSignature",
+        "type": "bytes"
       }
     ],
-    stateMutability: 'pure',
-    type: 'function',
-    constant: true
+    "stateMutability": "pure",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "agent",
+        "type": "address"
+      },
+      {
+        "internalType": "bytes",
+        "name": "timestamp",
+        "type": "bytes"
+      },
+      {
+        "internalType": "address",
+        "name": "delegate",
+        "type": "address"
+      },
+      {
+        "internalType": "bytes",
+        "name": "evidence",
+        "type": "bytes"
+      }
+    ],
+    "name": "validateEvidence",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "pure",
+    "type": "function",
+    "constant": true
   }
 ];
